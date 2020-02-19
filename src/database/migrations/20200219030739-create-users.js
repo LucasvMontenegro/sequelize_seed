b7b0ee820902e8f9dd4f7348435734086b7b0edb
Lucas Vinicius Vieira Montenegro <lucas.montenegro@somosphi.com>
2020-02-19 00:42:37 -0300

    model

--- src/database/migrations/20200219030739-create-users.js
+++ src/database/migrations/20200219030739-create-users.js
@@ -13,6 +13,9 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
